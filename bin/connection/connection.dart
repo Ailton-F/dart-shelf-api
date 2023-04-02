60c029afb4a9cac3db9347599765235539bf9473
Ailton Filho <ailtonxdz@gmail.com>
2023-04-02 12:18:44 -0300

    added some things but i'am migrating to jaguar framework

--- bin/connection/connection.dart
+++ bin/connection/connection.dart
@@ -1,23 +1,37 @@
 import 'package:mysql1/mysql1.dart';
-import '../../utils/customEnv.dart';
+import 'package:dotenv/dotenv.dart' as dotenv;
 
 class Connection {
   static Future<MySqlConnection> getMy() async {
-    String host = await CustomEnv.get<String>(key: 'db_host_mysql');
-    String user = await CustomEnv.get<String>(key: 'db_user_mysql');
-    int port = await CustomEnv.get<int>(key: 'db_port_mysql');
-    String password = await CustomEnv.get<String>(key: 'db_password_mysql');
-    String db = await CustomEnv.get<String>(key: 'db_schema_mysql');
-    
-    MySqlConnection connection =
-        await MySqlConnection.connect(ConnectionSettings(
+    final envVars = dotenv.load('.env');
+
+    final host = dotenv.env['db_host_mysql'];
+    final port = int.parse(dotenv.env['db_port_mysql'] ?? '3306');
+    final user = dotenv.env['db_user_mysql'];
+    final password = dotenv.env['db_password_mysql'];
+    final db = dotenv.env['db_schema_mysql'];
+
+    MySqlConnection conn = await MySqlConnection.connect(ConnectionSettings(
       host: host.toString(),
       user: user,
       port: port,
-      password: password,
       db: db,
     ));
 
-    return connection;
+    try {
+      // Attempt to execute a simple query to test the connection.
+      final results = await conn.query('SELECT 1');
+
+      // If the query was successful, print a message indicating that the connection was established.
+      print('Connection established.');
+    } catch (e) {
+      // If the query fails, print the error message.
+      print('Connection failed: ${e.toString()}');
+    } finally {
+      // Close the connection.
+      await conn.close();
+    }
+
+    return conn;
   }
 }
--- bin/server.dart
+++ bin/server.dart
@@ -8,10 +8,15 @@ import 'services/userServices.dart';
 // Configure routes.
 final _router = Router();
 
+// Configure services and controllers
+UserServices userServices = UserServices();
+final UserController userController = UserController(userServices);
+
 void main(List<String> args) async {
-  UserServices userServices = UserServices();
-  final UserController userController = UserController(userServices);
+  
+  // Configure routes
   _router.get('/', (request) => userController.getAllUsers(request));
+  
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
@@ -21,6 +26,7 @@ void main(List<String> args) async {
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8000');
   print(port);
+  
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }
